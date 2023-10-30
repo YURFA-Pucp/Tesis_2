2514ce3db9b46e911a8b767a02ecd9aae52aa332
YURFA-Pucp <ytoralva@pucp.edu.pe>
2023-10-29 20:48:27 -0500

    #R

--- Tesis_R/Mapa_cre_dist.R
+++ Tesis_R/Mapa_cre_dist.R
@@ -16,12 +16,12 @@ base=peruDep
 base1$distrito_propuesto=NULL
 
 
-distritos <- ggplot() + geom_sf(data = peru, aes(fill = Total_mb5), color='#c8c8c8', lwd = 0.001, show.legend = TRUE) +
-  scale_fill_gradient(low ="#F6F7F8",high = "#0C1E43") +
-  labs(fill = "Cantidad de distritos propuestos") +  # Etiqueta de la leyenda de colores
+distritos <- ggplot() + geom_sf(data = peru, aes(fill = Total_mb5_level), color='#c8c8c8', lwd = 0.001, show.legend = TRUE) +
+  scale_fill_manual(values = c("#F9FAFF","#A3A9BB","#606F96", "#0F255E")) +
+  labs(fill = "Demanda para la creación de distritos") +  # Etiqueta de la leyenda de colores
   guides(fill = guide_legend(title.position = "top", label.position = "right", title.theme = element_text(size = 11)))+
   theme(legend.position = "bottom") +# Posición de la leyenda
   theme_void() #quita las líneas
 
-choro3 <- distritos + geom_sf(data=base, color="#aaaaaa",fill="transparent",lwd = 1)
+choro3 <- distritos + geom_sf(data=base, color="#aaaaaa",fill="transparent",lwd = 0.9)
 choro3
